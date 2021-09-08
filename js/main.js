c7d54b8c3fc637476eaa5d7cd40fe111af4b2105
awmills25 <awmills25@gmail.com>
2021-09-08 09:54:40 -0400

    rearraged js for it to work

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 setInterval(updateTime, 1000); //updates time in milliseconds
 
 function updateTime() {
-    const time = new Date(); //holds current date and time
-
-    const hour = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
-
+    var time = new Date(); //holds current date and time
+    var hour = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
     am_pm = "AM"; //sets default time to morning
+
+    var clockDiv = document.getElementById('clock'); //links to html
 
     if (hour > 12) { // converts afternoon times to 12 hour clock
         hour -= 12;
@@ -22,10 +22,9 @@ function updateTime() {
     hour = hour < 10 ? "0" + hour : hour;
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    let currentTime = hour + ":" + minutes + ":" + seconds + am_pm;
-
-    document.getElementById("clock") = currentTime;
+    
+    //sets the text of the digital clock
+    clockDiv.innerText = hour + ":" + minutes + ":" + seconds + " " + am_pm; 
 }
 
 updateTime();
